befce63eb50554e71aa06152dddad5788ce76fbb
hanep <yudishan26@gmail.com>
2024-05-17 18:41:03 +0700

    implement create post

--- src/Pages/CreatePost.jsx
+++ src/Pages/CreatePost.jsx
@@ -1,5 +1,25 @@
+import { BiSend } from "react-icons/bi";
+import axiosClient from "../axios";
+import router from "../router";
+
 export default function CreatePost() {
+    function handleCreatePost(event) {
+        event.preventDefault()
+
+        axiosClient.post('/posts/create', {
+            body: event.target.body.value
+        }).then(response => {
+            if (response.status == 201) {
+                router.navigate('/')
+            }
+        })
+    }
     return (
-        <div>Create Post Page</div>
+        <div className="pt-2">
+            <form action="" onSubmit={handleCreatePost}>
+                <textarea name="body" rows={4} className="w-full p-2 outline-none rounded"></textarea>
+                <button type="submit" className="w-full h-10 flex justify-center items-center gap-1 rounded bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white">Post <BiSend /></button>
+            </form>
+        </div>
     )
 }
